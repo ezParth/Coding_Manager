292663c8709fe3615332bb5c9935540729e12f57
ezParth <parthsingh55m@gmail.com>
2025-03-25 16:33:54 +0530

    fixed

--- client/src/pages/landing_page/Cp_panel.tsx
+++ client/src/pages/landing_page/Cp_panel.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Codeforces from "./Codeforces";
 import Leetcode from "./Leetcode";
 
-function Cp_panel() {
+const Cp_panel: React.FC = () => {
   return (
     <div className="w-full h-116 flex items-center border-t border-b border-gray-600 shadow-md">
       <div className="flex w-full">
