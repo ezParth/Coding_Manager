bd2b4ebae0e045f3860c012a0c95acb2ce1e55d4
ezParth <parthsingh55m@gmail.com>
2025-03-28 23:04:14 +0530

    added project login-3

--- client/src/pages/landing_page/ShowProject.tsx
+++ client/src/pages/landing_page/ShowProject.tsx
@@ -5,7 +5,7 @@ const ShowProject: React.FC = () => {
     return(
         <div>
             <div className="w-full h-full flex item-center justify-center m-5 flex-col mb-5 pb-20" >
-                <h1 className="text-black font-sans text-4xl font-bold flex items-center justify-center m-5 mb-10">YOUR PROJECTS</h1>
+                <h1 className="text-black font-sans text-4xl font-bold flex items-center justify-center m-5 mb-10">YOUR PROJECTS(Under Construction:)</h1>
                 <IndividualProjects />
             </div>
         </div>
